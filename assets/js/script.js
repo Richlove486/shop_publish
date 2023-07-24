9200adf2523bb0915fe619a0ebc981a708cbded7
abc073 <abc073@nate.com>
2023-07-24 23:50:01 +0900

    섹션02 푸시

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
-$document.ready(function () {});
+$(document).ready(function () {
+    /* 섹션02 슬라이드 */
+    var swiper = new Swiper('.mySwiper', {
+        slidesPerView: 1,
+        spaceBetween: 16,
+        speed: 800,
+        loop: true,
+        loopedSlides: 1,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1.1,
+                spaceBetween: 16,
+            },
+            500: {
+                slidesPerView: 2.3,
+                spaceBetween: 16,
+            },
+            1024: {
+                slidesPerView: 2.5,
+                spaceBetween: 16,
+            },
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
