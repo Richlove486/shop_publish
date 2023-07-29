d0e3fd51aff6b6bc27b4ec9796bbadb2eaa26a8c
abc073 <abc073@nate.com>
2023-07-29 18:24:56 +0900

    섹션03 스토리 푸시

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
         spaceBetween: 16,
         speed: 800,
         loop: true,
-        loopedSlides: 1,
+        loopedSlides: 3,
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
@@ -27,6 +27,47 @@ $(document).ready(function () {
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
+        },
+    });
+
+    // 섹션03 갤러리 썸네일
+    var thumbSwiper = new Swiper('.gallery_thumb', {
+        spaceBetween: 0,
+        allowTouchMove: true,
+        loop: true,
+        breakpoints: {
+            768: {
+                direction: 'vertical',
+            },
+            480: {
+                // loopedSlides: 2.5,
+            },
+        },
+    });
+    // 섹션03 갤러리 메인
+    var mainSwiper = new Swiper('.gallery_main', {
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true,
+        },
+        slideToClickedSlide: true,
+        loop: true,
+        // breakpoints: {
+        //     1024: {
+        //         slidesPerView: 5, // 한 화면에 보일 섬네일 개수
+        //         loopedSlides: 5 // 위 섬네일 개수와 동일한 수치 설정
+        //     },
+        //     768: {
+        //         slidesPerView: 3.5,
+        //         loopedSlides: 3.5
+        //     },
+        //     480: {
+        //         slidesPerView: 2.5,
+        //         loopedSlides: 2.5
+        //     }
+        // }
+        thumbs: {
+            swiper: thumbSwiper,
         },
     });
 });
